185aef062c5609de2366cf7e820062bfde307ee4
eodbrent <33612870+eodbrent@users.noreply.github.com>
2024-04-16 07:44:35 -0500

    Add files via upload

---
+++ krous_cs475_topic6.hs
@@ -0,0 +1,25 @@
+{-
+Brent Krous
+CS475, Computation Theory
+Week 5/Topic 5, Lambda Calculus
+14 April 2024
+-}
+import Text.Show.Functions
+
+add = \x -> \y -> \a -> \b -> (x a) (y a b)
+
+s :: ((a -> a) -> a -> a) -> (a -> a) -> a -> a
+s = \x -> \y -> \z -> y (x y z)
+
+true = \x -> \y -> x
+false = \x -> \y -> y
+
+ifThenElse :: (c -> c -> c) -> c -> c -> c
+ifThenElse boolExpr trueExpr falseExpr = boolExpr trueExpr falseExpr
+
+church 0 = \x -> \y -> y -- provided
+church n = \f -> \x -> f (church (n - 1) f x)
+-- peano PROVIDED --
+peano lexp = lexp (\xs -> ('S':xs)) "0"
+
+myLambdaProgram = \m n -> m (\_ -> add n (church 7)) (add n (church 3))
